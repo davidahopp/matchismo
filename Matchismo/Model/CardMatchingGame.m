e913c3d64d7981313ed552d86a9ada703e29c945
David Hopp <davidahopp@gmail.com>
2013-02-15 16:59:49 -0600

    working on building card matching game

--- Matchismo/Model/CardMatchingGame.m
+++ Matchismo/Model/CardMatchingGame.m
@@ -22,13 +22,50 @@
     return _cards;
 }
 
+#define MATCH_BONUS 4
+#define MISMATCH_PENALTY 2
+#define FLIP_COST 1
+
+- (void)flipCardAtIndex:(NSUInteger)index{
+    Card *card = [self cardAtIndex:index];
+    
+    if (card && !card.isUplayable){
+        
+        for (Card *otherCard in self.cards){
+            if (otherCard.isFaceUp && !otherCard.isUplayable){
+                int matchScore = [card match:@[otherCard]];
+                if(matchScore){
+                    card.unplayable = YES;
+                    otherCard.unplayable = YES;
+                    self.score += matchScore * MATCH_BONUS;
+                } else {
+                    otherCard.faceUp = NO;
+                    self.score -= MISMATCH_PENALTY;
+                }
+                break;
+            }
+        }
+        self.score -= FLIP_COST;
+        card.faceUp = !card.isFaceUp;
+    }
+}
+
+- (Card *)cardAtIndex:(NSUInteger)index{
+    return (index < [self.cards count]) ? self.cards[index] : nil;
+}
+
 - (id) initWithCardCoutn:(NSUInteger)count usingDeck:(Deck *)deck{
     self = [super init];
     
     if(self){
         for(int i = 0; i < count; i++){
             Card *card = [deck drawRandomCard];
-            self.cards[i] = card;
+            if(card){
+                self.cards[i] = card;
+            } else {
+                self = nil;
+                break;
+            }
         }
     }
     
